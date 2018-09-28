d81ee5c890aaa991392dda169d0bd8c9e81eb4b3
Muggle Potter <sanjanatripathi007@gmail.com>
2018-09-28 09:45:11 -0500

    no major changes - indentation

--- client/src/components/Layout/Home.js
+++ client/src/components/Layout/Home.js
@@ -1,22 +1,58 @@
-import React from "react";
-import {Map} from "../Custom";
+import React, {Component} from "react";
+import { Map1 } from "../Custom";
 
-export default (props) => {
-    const styles = {
-        containerstyle : {
+class MapContainer extends Component {
+    state = {
+        markers: [
+            {
+                position: {
+                    lat: 30.2672,
+                    lng: -97.7431
+                }
+            }
+        ]
+    }
+    styles = {
+        containerstyle: {
             height: 100
         },
         mapstyle: {
             height: 100
         }
     }
-    return(
-        <div>Home
-            <Map 
-                containerElement={<div style={styles.containerstyle} />}
-                mapElement={<div style={styles.mapstyle} />}
-            />
-        </div>
-        
-    )
+    render() {
+        return (
+            <div>Home
+                <Map1
+                    containerElement={<div style={this.styles.containerstyle} />}
+                    mapElement={<div style={this.styles.mapstyle} />}
+                    markers={this.state.markers}
+                />
+            </div>
+
+        )
+    }
 }
+
+export default MapContainer;
+
+// export default (props) => {
+//     const styles = {
+//         containerstyle: {
+//             height: 100
+//         },
+//         mapstyle: {
+//             height: 100
+//         }
+//     }
+//     return (
+//         <div>Home
+//             <Map
+//                 containerElement={<div style={styles.containerstyle} />}
+//                 mapElement={<div style={styles.mapstyle} />}
+//                 markers={{ lat: 30.2672, lng: -97.7431 }}
+//             />
+//         </div>
+
+//     )
+// }
