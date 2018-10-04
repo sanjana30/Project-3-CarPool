aa301770eb466c7fe03fe261e6623f6bddedd931
Muggle Potter <sanjanatripathi007@gmail.com>
2018-10-04 10:41:36 -0500

    added internal styling to NavDashboard

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -10,11 +10,7 @@ const styles = {
 const Nav = () => (
     <nav className="navbar navbar-light" style={styles.navStyle}>
         <a className="navbar-brand" href="#">
-<<<<<<< HEAD
-        <img src={Logo} width="30" height="30" alt="logo" />
-=======
             <img src="../images/logo.png" width="30" height="30" alt="logo" />
->>>>>>> f1d718898d75184cae6dfefefef52fed6cc31fea
         </a>
     </nav>
 );
